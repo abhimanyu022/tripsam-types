ca2c7d9dcbdd6befd15af6b155131b9128d7b547
abhimanyu022 <abhy022@gmail.com>
2025-12-25 12:13:38 +0530

    release v:1.0.77

--- packages/onboarding/src/common/document.ts
+++ packages/onboarding/src/common/document.ts
@@ -1,3 +1,19 @@
-import z from "zod";
+import z, { url } from "zod";
 
-export const SafariDocuments = z.enum(['aadhaar_front', 'aadhaar_back', 'driving_license_front', 'driving_license_back', 'vehicle_rc', 'pan_card']);
+export const SafariDocuments = z.enum(
+  ['aadhaar_front',
+    'aadhaar_back',
+    'driving_license_front',
+    'driving_license_back',
+    'vehicle_rc',
+    'pan_card']);
+
+export type SafeSafariDocuments = z.infer<typeof SafariDocuments>;
+
+export const get = z.object({
+  id: z.string(),
+  type: SafariDocuments,
+  url: z.string()
+});
+
+export type SafeGet = z.infer<typeof get>;
