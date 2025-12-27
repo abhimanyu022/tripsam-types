0f6950ff5a709504508b4565451d67329962e73a
abhimanyu022 <abhy022@gmail.com>
2025-12-27 16:22:48 +0530

    feat: release v:1.0.78

--- packages/onboarding/src/safari/driver-details.ts
+++ packages/onboarding/src/safari/driver-details.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const languageKnownEnum = z.enum(['english', 'hindi', 'gujurati', 'marathi']);
+export const languageKnownEnum = z.enum(['english', 'hindi', 'gujarati', 'marathi']);
 
 export const get = z.object({
   id: z.string(),
